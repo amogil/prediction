a4643c9f02e126fb4c94ec49c0c2e986b0dea01e
Alexey Mogilnikov <alexey@mogilnikov.name>
2013-11-29 20:03:48 +0600

    - little finder fix

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
-# Cleanup
-rm -f tmp/*.js
 # Compile coffeescript
-coffee -c -o tmp src/*.coffee
-
-# Combine, minify and append libs
-#java -jar bin/yuicompressor-2.4.8.jar tmp/*.js lib/*.js -o deploy/latest/prediction.js
-# Only combine and append libs
-cat tmp/*.js > deploy/latest/prediction.js
+coffee --compile --join deploy/latest/prediction.js src/*.coffee
+# Concat libs
 cat lib/*.js >> deploy/latest/prediction.js
-
-# Cleanup
-rm -f tmp/*.js
+# Minify js
+java -jar bin/yuicompressor-2.4.8.jar deploy/latest/prediction.js -o deploy/latest/prediction.js
