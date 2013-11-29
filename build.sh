d3ffe22ba85e38218141c29ce27d9f598b5da3e3
Alexey Mogilnikov <alexey@mogilnikov.name>
2013-11-29 20:06:57 +0600

    disable minifying

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ coffee --compile --join deploy/latest/prediction.js src/*.coffee
 # Concat libs
 cat lib/*.js >> deploy/latest/prediction.js
 # Minify js
-java -jar bin/yuicompressor-2.4.8.jar deploy/latest/prediction.js -o deploy/latest/prediction.js
+# java -jar bin/yuicompressor-2.4.8.jar deploy/latest/prediction.js -o deploy/latest/prediction.js
